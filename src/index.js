da35ccff1a2566da755cf6150459f263f3668009
prakash-rahangdale <prakash170817@gmail.com>
2021-04-04 22:54:08 +0530

    converting styling to jsx format

--- src/index.js
+++ src/index.js
@@ -4,7 +4,21 @@ import ReactDOM from 'react-dom';
 
 //create a react component
 const App = () => {
-    return <div>this is the  first code to show the react app</div>
+    return (
+
+    //html styling
+    // <div>
+    //     <label class="label" for="name">Enter Name:</label>
+    //     <input id="name" type="text"/>
+    //     <button style="background-color: blue; color: white;">submit</button>
+    // </div>
+    
+    <div>
+        <label class="label" for="name">Enter Name:</label>
+        <input id="name" type="text"/>
+        <button style={{backgroundColor: 'blue', color: 'white'}}>submit</button>
+    </div>
+    );
 };
 
 //take a react component and show it on the screen
